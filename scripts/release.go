dc1ce776e57c23107af109bfdce0d19de8bc601e
Subhajit Ghosh <isubuz.g@gmail.com>
2023-11-22 16:11:02 +0000

    next release

--- scripts/release.go
+++ scripts/release.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -14,6 +15,8 @@ func check(e error) {
 		panic(e)
 	}
 }
+
+var ENVS = []string{"dev", "staging", "qa"}
 
 func fileExists(fileName string) bool {
 	_, err := os.Stat(fileName)
@@ -42,6 +45,14 @@ func main() {
 	numApps, err := strconv.Atoi(numAppsEnvVar)
 	check(err)
 
+	var syncEnvs []string
+	releaseEnvsVar := strings.TrimSpace(os.Getenv("RELEASE_ENVS"))
+	if len(releaseEnvsVar) == 0 {
+		syncEnvs = []string{}
+	} else {
+		syncEnvs = strings.Split(releaseEnvsVar, ",")
+	}
+
 	currReleaseId, err := strconv.Atoi(releaseIdEnvVar)
 	fmt.Printf("Found current release id (%d)\n", currReleaseId)
 	check(err)
@@ -49,41 +60,51 @@ func main() {
 	nextReleaseId := currReleaseId + 1
 	fmt.Printf("Generating release for id (%d)\n", nextReleaseId)
 
-	for i := 1; i <= numApps; i++ {
-		app := fmt.Sprintf("gb-%d", i)
-		valuesFilePath := fmt.Sprintf("../helm-values/dev/%s.yaml", app)
-		if fileExists(valuesFilePath) {
-			valuesFile, err := os.ReadFile(valuesFilePath)
+	for _, env := range ENVS {
+		for i := 1; i <= numApps; i++ {
+			envDir := fmt.Sprintf("../helm-values/%s", env)
+			err = os.MkdirAll(envDir, os.ModePerm)
 			check(err)
 
-			var c conf
-			err = yaml.Unmarshal(valuesFile, &c)
-			check(err)
+			app := fmt.Sprintf("gb-%d", i)
+			valuesFilePath := fmt.Sprintf("%s/%s.yaml", envDir, app)
+			if fileExists(valuesFilePath) {
+				if len(syncEnvs) > 0 && !slices.Contains(syncEnvs, env) {
+					continue
+				}
+				valuesFile, err := os.ReadFile(valuesFilePath)
+				check(err)
 
-			// set next release id
-			c.ReleaseId = nextReleaseId
+				var c conf
+				err = yaml.Unmarshal(valuesFile, &c)
+				check(err)
 
-			// write file
-			d, err := yaml.Marshal(&c)
-			check(err)
+				// set next release id
+				c.ReleaseId = nextReleaseId
 
-			fmt.Printf("Writing next release id for app (%s)\n", app)
-			err = os.WriteFile(valuesFilePath, d, 0644)
-			check(err)
+				// write file
+				d, err := yaml.Marshal(&c)
+				check(err)
 
-		} else {
-			fmt.Printf("Generating new values file for app (%s)\n", app)
-			c := conf{
-				Name: app,
-				Service: service{
-					Port: 30000 + i,
-				},
-				ReleaseId: nextReleaseId,
+				fmt.Printf("Writing next release id for app (%s) and env (%s)\n", app, env)
+				err = os.WriteFile(valuesFilePath, d, 0644)
+				check(err)
+
+			} else {
+				fmt.Printf("Generating new values file for app (%s) and env (%s)\n", app, env)
+				c := conf{
+					Name: app,
+					Service: service{
+						Port: 30000 + i,
+					},
+					ReleaseId: nextReleaseId,
+				}
+				d, err := yaml.Marshal(&c)
+				check(err)
+				err = os.WriteFile(valuesFilePath, d, 0644)
+				check(err)
 			}
-			d, err := yaml.Marshal(&c)
-			check(err)
-			err = os.WriteFile(valuesFilePath, d, 0644)
-			check(err)
 		}
+
 	}
 }
